aef24dae64f3646fdec76a046c48fc21cfdd18bd
TamuraYumeha <c612201003@tokushima-u.ac.jp>
2022-09-08 22:38:01 +0900

    制限時間がきた時にゲーム画面が停止するようにした

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour {
     private Transform PlayerTransform;
     private Transform CameraTransform;
     // Use this for initialization
+
+    public float stoptime = 180.0f;
+
     void Start () {
 
         PlayerTransform = transform.parent;
@@ -19,33 +22,45 @@ public class PlayerController : MonoBehaviour {
 	
     // Update is called once per frame
     void Update () {
-        float X_Rotation = Input.GetAxis("Mouse X");
-        float Y_Rotation = Input.GetAxis("Mouse Y");
-        PlayerTransform.transform.Rotate(0, X_Rotation, 0);
-        CameraTransform.transform.Rotate(-Y_Rotation, 0, 0);
+
+        stoptime -= Time.deltaTime;
+
+        if (stoptime > 0)
+        {
+            float X_Rotation = Input.GetAxis("Mouse X");
+            float Y_Rotation = Input.GetAxis("Mouse Y");
+            PlayerTransform.transform.Rotate(0, X_Rotation, 0);
+            CameraTransform.transform.Rotate(-Y_Rotation, 0, 0);
 
 
-        float angleDir = PlayerTransform.transform.eulerAngles.y * (Mathf.PI / 180.0f);
-        Vector3 dir1 = new Vector3(Mathf.Sin(angleDir), 0, Mathf.Cos(angleDir));
-        Vector3 dir2 = new Vector3(-Mathf.Cos(angleDir), 0, Mathf.Sin(angleDir));
+            float angleDir = PlayerTransform.transform.eulerAngles.y * (Mathf.PI / 180.0f);
+            Vector3 dir1 = new Vector3(Mathf.Sin(angleDir), 0, Mathf.Cos(angleDir));
+            Vector3 dir2 = new Vector3(-Mathf.Cos(angleDir), 0, Mathf.Sin(angleDir));
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                PlayerTransform.transform.position += dir1 * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                PlayerTransform.transform.position += dir2 * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                PlayerTransform.transform.position += -dir2 * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                PlayerTransform.transform.position += -dir1 * speed * Time.deltaTime;
+            }
+        }
         
-        
-        if (Input.GetKey(KeyCode.W))
+        if (stoptime <= 0)
         {
-            PlayerTransform.transform.position += dir1 * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            PlayerTransform.transform.position += dir2 * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            PlayerTransform.transform.position += -dir2 * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            PlayerTransform.transform.position += -dir1 * speed * Time.deltaTime;
-        }
+            Time.timeScale = 0;
+            PlayerTransform.transform.Rotate(0, 0, 0);
+            CameraTransform.transform.Rotate(0, 0, 0);
 
+        }
     }
 }
