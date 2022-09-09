4e49364f84f4f4b834e84a502e55fab377d2b286
ryt31 <ryota.game10@gmail.com>
2022-09-10 00:36:33 +0900

    リザルト画面の完成

---
+++ Assets/Script/ResultModel.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ResultModel : MonoBehaviour
+{
+    public int ExplosionCount { get; set; } = 1043;
+    public int ExplosionSameCount { get; set; } = 108;
+    public int Score { get; set; } = 1999999;
+}
