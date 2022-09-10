8e07fb0ba4d58a4979ed951e0067fd6d0d65a7ab
ryt31 <ryota.game10@gmail.com>
2022-09-10 17:06:53 +0900

    グレネードアイテムがぷかぷかなるようにした

--- Assets/Script/GranadeItem.cs
+++ Assets/Script/GranadeItem.cs
@@ -1,10 +1,19 @@
 using System;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using UnityEngine;
 
 public class GranadeItem : MonoBehaviour
 {
     public Action Callback { get; set; }
-    
+    private TweenerCore<Vector3, Vector3, VectorOptions> move;
+
+    private void Start()
+    {
+        move = transform.DOLocalMove(transform.localPosition + Vector3.down, 1.0f).SetLoops(-1, LoopType.Yoyo);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -16,6 +25,7 @@ public class GranadeItem : MonoBehaviour
 
     private void OnDestroy()
     {
+        move.Kill();
         Callback.Invoke();
     }
 }
